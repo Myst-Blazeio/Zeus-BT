c53d21a7f35d19fde8eacd42a3ca4efa42940e05
Myst-Blazeio <pushkarpan03@gmail.com>
2024-10-18 20:18:31 +0530

    push

--- JS/mentor-dashboard.js
+++ JS/mentor-dashboard.js
@@ -6,3 +6,8 @@ statistics.forEach(statistic => {
     const originalNumber = numberElement.textContent;
     console.log(`Original number for this statistic: ${originalNumber}`); // Optional: for debugging
 });
+
+function scrollToSection() {
+            const target = document.getElementById('schedule');
+            target.scrollIntoView({ behavior: 'smooth' });
+        }
